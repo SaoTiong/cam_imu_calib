6741d5eda94352b0af80c1bc0833df2b426092bc
SaoTiong <tong@dwe.ai>
2026-02-27 18:09:36 -0800

    no bags

--- build/adafruit_icm20948_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tong/DWE/DWECal_ws/src/adafruit_icm20948_ros
-# Build directory: /home/tong/DWE/DWECal_ws/build/adafruit_icm20948_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
